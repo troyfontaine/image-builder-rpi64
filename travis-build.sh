028da253850246efe9cdfb7c644235872b2d214f
Troy Fontaine <troyfontaine@users.noreply.github.com>
2018-08-03 23:16:08 -0500

    Fix typo (#6)
    * Fix typo
    
    * Modifies release scenario
    
    * Corrects if statement
    
    * Upgrades GHR
    
    Signed-off-by: Troy Fontaine <tfontaine@troyfontaine.com>

--- travis-build.sh
+++ travis-build.sh
@@ -21,13 +21,14 @@ fi
 set -x
 
 # Create a build number
-export BUILD_NR="$(date '+%Y%m%d-%H%M%S')"
+BUILD_NR="$(date '+%Y%m%d-%H%M%S')"
+export "BUILD_NR=$BUILD_NR"
 echo "BUILD_NR=$BUILD_NR"
 
 # Run build steps
 # Create build directory
-BUILD_DEST=builds/$BUILD_NR
-mkdir -p $BUILD_DEST
+BUILD_DEST="builds/$BUILD_NR"
+mkdir -p "$BUILD_DEST"
 
 # Build image
 VERSION=v$BUILD_NR make shellcheck
@@ -36,16 +37,20 @@ VERSION=v$BUILD_NR make sd-image
 # Test image
 VERSION=v$BUILD_NR make test
 
-# Prep the build to go to Github releases if we're on the master branch
-if [ "$TRAVIS_BRANCH" == "master"]; then
-  # Move artifacts to build dest
-  mv hypriotos-rpi64* $BUILD_DEST/
+# Release if not a pull request
+if [ -z "$TRAVIS_PULL_REQUEST_BRANCH" ]; then
+  # Release if on Master Branch
+  if [ "$TRAVIS_BRANCH" == "master" ]; then
+    # Move artifacts to build dest
+    mv hypriotos-rpi64* "$BUILD_DEST/"
 
-  # Deploy to GitHub releases
-  export GIT_TAG=v$BUILD_NR
-  export GIT_RELTEXT="Auto-released by [Travis-CI build #$TRAVIS_BUILD_NUMBER](https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID)"
-  curl -sSL https://github.com/tcnksm/ghr/releases/download/v0.5.4/ghr_v0.5.4_linux_amd64.zip > ghr.zip
-  unzip ghr.zip
-  ./ghr --version
-  ./ghr --debug -u troyfontaine -b "$GIT_RELTEXT" $GIT_TAG builds/$BUILD_NR/
+    # Deploy to GitHub releases
+    export GIT_TAG=v$BUILD_NR
+    export GIT_RELTEXT="Auto-released by [Travis-CI build #$TRAVIS_BUILD_NUMBER](https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID)"
+    curl -sSL https://github.com/tcnksm/ghr/releases/download/v0.10.2/ghr_v0.10.2_linux_amd64.zip > ghr.zip
+    unzip ghr.zip
+    ./ghr --version
+    ./ghr --debug -u troyfontaine -b "$GIT_RELTEXT" "$GIT_TAG builds/$BUILD_NR/"
+  fi
+
 fi
